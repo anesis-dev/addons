a4453105dfef6a57def9842e54d27737a39fdb0d
Maksym Zhuk <147655559+Maksym-Zhuk@users.noreply.github.com>
2026-07-17 11:40:03 +0300

    Refactor PrismaService to include OnModuleDestroy

--- nest-prisma-v7/files/install/prisma.service.ts
+++ nest-prisma-v7/files/install/prisma.service.ts
@@ -1,17 +1,25 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaClient } from 'src/generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
+export class PrismaSercice
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  constructor(private readonly config: ConfigService) {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString: config.get('DATABASE_URL'),
     });
     super({ adapter });
   }
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
